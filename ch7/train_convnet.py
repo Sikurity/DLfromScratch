0b7c601f69ce794e16db4f2057e03bf6953057be
YeongSik Lee <leejk9592@naver.com>
2020-01-11 16:45:20 +0900

    Modify import path for simple_convnet

--- ch7/train_convnet.py
+++ ch7/train_convnet.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
-from simple_convnet import SimpleConvNet
+from ch7.simple_convnet import SimpleConvNet
 from common.trainer import Trainer
 
 # Load Data
@@ -13,15 +13,15 @@ save_file = dataset_dir + "/mnist.pkl"
 (x_train, t_train), (x_test, t_test) = load_mnist(dataset_dir, save_file, flatten=False)
 
 # If training takes a long time, use only part of datas
-#x_train, t_train = x_train[:5000], t_train[:5000]
-#x_test, t_test = x_test[:1000], t_test[:1000]
+# x_train, t_train = x_train[:5000], t_train[:5000]
+# x_test, t_test = x_test[:1000], t_test[:1000]
 
 max_epochs = 20
 
-network = SimpleConvNet(input_dim=(1,28,28), 
-                        conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+network = SimpleConvNet(input_dim=(1, 28, 28),
+                        conv_param={'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
                         hidden_size=100, output_size=10, weight_init_std=0.01)
-                        
+
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
                   epochs=max_epochs, mini_batch_size=100,
                   optimizer='Adam', optimizer_param={'lr': 0.001},
